de4be8f0400ad65f547a3fb03c32c81be5e80a28
Victoria G <125226074+victoria-cg@users.noreply.github.com>
2024-03-23 16:16:57 -0500

    added environment variables, imported sequelize to connection.js, imported connection.js to server.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
+//this imports/uses the .env file with the environment variables
 require('dotenv').config();
 
+//this imports sequelize so we can work with the SQL database 
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
