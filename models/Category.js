fe94f38075cf81f274f6a6f370379e5eff369c41
Victoria G <125226074+victoria-cg@users.noreply.github.com>
2024-03-23 21:17:28 -0500

    added column names, validations, and constraints to files in the models directory

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,23 @@
+//imports sequelize and creates a new class that serves as the sequelize model for the data
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
-
+//category is an extension if the model, and inherits its methods, like 'init'
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define columns: column name contains the properties/data types of the column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
