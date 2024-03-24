67ce1d2f0dac3fc24e246c74db31e2f7296d09fc
Victoria G <125226074+victoria-cg@users.noreply.github.com>
2024-03-23 22:28:58 -0500

    added assocication methods for sequelize models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Product belongsTo Category: categories have many Products but products belong to a single Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+//Categories have many Products
+//onDelete: 'CASCADE' means that if the parent Category is deleted, the  products referencing that category will also be deleted
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+// Product belongToMany Tag (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+// Tag belongToMany Product (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
 
 module.exports = {
   Product,
